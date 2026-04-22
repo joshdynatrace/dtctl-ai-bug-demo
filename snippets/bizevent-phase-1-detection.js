d51b60bc8a383dc85b00d179e45b93f6201e41e4
joshDynatrace <joshua.hendrick@dynatrace.com>
2026-04-22 14:41:52 -0700

    Deployed 4cb5e5e with MkDocs version: 1.6.1

---
+++ snippets/bizevent-phase-1-detection.js
@@ -0,0 +1,7 @@
+<script>
+var CURRENT_PAGE = "4. phase-1-detection";
+
+document.addEventListener('DOMContentLoaded', function() {
+  dynatrace.sendBizEvent('page_load', {"page": CURRENT_PAGE});
+});
+</script>
---
+++ snippets/bizevent-phase-3-remediation.js
@@ -0,0 +1,7 @@
+<script>
+var CURRENT_PAGE = "6. phase-3-remediation";
+
+document.addEventListener('DOMContentLoaded', function() {
+  dynatrace.sendBizEvent('page_load', {"page": CURRENT_PAGE});
+});
+</script>
